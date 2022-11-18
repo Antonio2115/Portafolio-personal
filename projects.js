90e8dc620feb5aa302c348cc7b7e261170ec54bf
Jose Antonio VA <joseantoniova159@gmail.com>
2022-11-18 17:40:50 -0600

    Begin and about me
    Integración de apartados de inicio y de descripción sobre mi

---
+++ projects.js
@@ -0,0 +1,10 @@
+const projects =[
+    //Proyecto button mashing
+    {},
+    //Proyecto makecube
+    {},
+    //Proyecto separador de primeras aguas automatizado
+    {},
+    //Proyecto portafolio personal
+    {}
+];
