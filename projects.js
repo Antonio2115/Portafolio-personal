d41ea9b24db09f5948617f26e88746b306311715
Jose Antonio VA <joseantoniova159@gmail.com>
2023-05-24 20:02:08 -0600

    Updating of information

--- projects.js
+++ projects.js
@@ -9,35 +9,49 @@ const projects =[
         "https://drive.google.com/uc?export=view&id=1L4l33_eo1yVmr37P_gZNP9mcg9inBwKO",
         "https://drive.google.com/uc?export=view&id=160iwtgeh2fxTjSKHyWVrqeckcu5SkJkC"
     ],
-    description: "Button mashing es un juego realizado a partir de Visual Basic.net y Arduino. El juego consiste en una competición entre dos jugadores que tienen como objetivo sumar 5 puntos. La manera de conseguir estos puntos, consiste en presionar su botón cuando el juego de una advertencia antes que su adversario. En caso de presionar el botón antes o cuando no esta la advertencia se le resta 1 punto.",
+    description: "Button mashing es un juego realizado a partir de Visual Basic.net y Arduino. El juego consiste en una competición entre dos jugadores que tienen como objetivo sumar 5 puntos. La manera de conseguir estos puntos, consiste en presionar su botón cuando el juego de una advertencia antes que su adversario. En caso de presionar el botón antes o cuando no está la advertencia se le resta 1 punto.",
     toolsUsed: [
         "Arduino",
         "Visual Basic.NET",
         "Electronica básica"
     ],
     documentation: "https://drive.google.com/file/d/1l42erKeorZyIpfe6OZGlR6IoId8i39j0/view?usp=sharing",
+    PreviewOnline: "",
+    RepositoryInGitHub: "",
+    downloadProject:""
   },
   //Proyecto makecube
   {
     name:"MAKECUBE",
     category:"Factores de trabajo",
     images: [
-      "https://drive.google.com/uc?export=view&id=1Jt9GQCSC4-cd3JB6ZxSqe_KayuP-ihBG"
+      "https://drive.google.com/uc?export=view&id=1Jt9GQCSC4-cd3JB6ZxSqe_KayuP-ihBG",
+      "https://drive.google.com/uc?export=view&id=1ihkBWfobYOK_Q-_pTGLpIY0yBr-Rs3j3",
+      "https://drive.google.com/uc?export=view&id=1ZCIieT9s7eGUj5sOyHSj9K-HWRuswnzR",
+      "https://drive.google.com/uc?export=view&id=1ZKXouKML6dEJeo0qWf6YpyRygvF3KGZX",
+      "https://drive.google.com/uc?export=view&id=1dBUVIyb01asFxhzQnlCQnBtpILG4fRko"
     ],
-    description:"MAKECUBE es una empresa ficticia que se realizo como proyecto para analizar los diferentes factores de seguridad y ergonomía para mantener la integridad de trabajadores y clientes. Para la realización de estos análisis se realizo diferentes cálculos de ergonomía y se recreo los entornos de trabajo a partir de los softwares AutoCAD y SketchUp.",
+    description:"MAKECUBE es una empresa ficticia que se realizó como proyecto para analizar los diferentes factores de seguridad y ergonomía para mantener la integridad de trabajadores y clientes. Para la realización de estos análisis se realizó diferentes cálculos de ergonomía y se recreó los entornos de trabajo a partir de los software AutoCAD y SketchUp.",
     toolsUsed: [
       "AutoCAD",
       "SOLIDWORKS",
       "Sketch Up"
    ],
-    documentation:"https://drive.google.com/file/d/1bfHKZaqej7noppzG57xmqK-HHvXtqWA4/view?usp=sharing"
+    documentation:"https://drive.google.com/file/d/1bfHKZaqej7noppzG57xmqK-HHvXtqWA4/view?usp=sharing",
+    PreviewOnline: "",
+    RepositoryInGitHub: "",
+    downloadProject:""
   },
   //Proyecto separador de primeras aguas automatizado
   {
     name:"Separador Automatizado",
     category:"Residencia Profesional",
     images: [
-      "https://drive.google.com/uc?export=view&id=1G2nwhzi0RzZHODW9xV7g_uEP5MaJWe_N"
+      "https://drive.google.com/uc?export=view&id=1G2nwhzi0RzZHODW9xV7g_uEP5MaJWe_N",
+      "https://drive.google.com/uc?export=view&id=12YkbCzzNpdwMsYRzdYcRTcjJuD3NEIMx",
+      "https://drive.google.com/uc?export=view&id=1kHGz3BD8dKpUtlldDy-2dt4mCI1UbMAL",
+      "https://drive.google.com/uc?export=view&id=1tBLgEZLxBrJvoj_765dRYcPd3b7r1zZo",
+      "https://drive.google.com/uc?export=view&id=1JCIucjbTDaLu4XC6W0__nIAJXqenyjsS"
     ],
     description:"El presente proyecto es un separador de primeras aguas automatizado que forma parte de un sistema de captación de agua de lluvia en la Escuela Superior Normal Veracruzana “DR. Manuel Suárez Trujillo”. La automatización del presente separador de aguas consiste en el vaciado automatizado, ya sea para las cañerías o hacia la cisterna dependiendo de las condiciones del agua.",
     toolsUsed: [
@@ -46,7 +60,10 @@ const projects =[
       "Arduino",
       "Electronica básica"
     ],
-    documentation:"https://drive.google.com/file/d/1hLiKRU3Yg_dsho0ydTn-ZKhYnp7_S0O8/view?usp=sharing"
+    documentation:"https://drive.google.com/file/d/1hLiKRU3Yg_dsho0ydTn-ZKhYnp7_S0O8/view?usp=sharing",
+    PreviewOnline: "",
+    RepositoryInGitHub: "",
+    downloadProject:""
   }
 ];
 
@@ -54,9 +71,13 @@ const projects =[
 // {
 //   name:"",
 //   category:"",
-//   images: [""],
+//   images: ["
+//      "https://drive.google.com/uc?export=view&id= ID en Drive"
+//   "],
 //   description:"",
 //   toolsUsed: [""],
 //   documentation:"",
+//   PreviewOnline: "",
+//   RepositoryInGitHub "",
 //   downloadProject:""
 // }
